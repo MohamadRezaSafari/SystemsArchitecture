ce93f68976a1ff4aea5d8a556fa4570dc29ea104
Reza <mrdev1020@gmail.com>
2024-03-14 17:25:34 +0330

    Clean - Infrastructure

---
+++ Architecture/Clean/Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+
+    public async Task<int> SaveChangesAsync()
+    {
+        return await base.SaveChangesAsync();
+    }
+}
---
+++ Architecture/Clean/Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,46 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories;
+
+public class UserRepository : IUserRepository
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public UserRepository(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<User> GetUserById(int id)
+    {
+        return await _dbContext.Users.FindAsync(id);
+    }
+
+    public async Task<List<User>> GetAllUsers()
+    {
+        return await _dbContext.Users.ToListAsync();
+    }
+
+    public async Task CreateUser(User user)
+    {
+        await _dbContext.Users.AddAsync(user);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task UpdateUser(User user)
+    {
+        _dbContext.Users.Update(user);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task DeleteUser(int id)
+    {
+        var user = await GetUserById(id);
+        _dbContext.Users.Remove(user);
+        await _dbContext.SaveChangesAsync();
+    }
+}
+
