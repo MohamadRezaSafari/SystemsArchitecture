bc8e7d7af1c5ec53ff0946993079527d087dc12a
Reza <mrdev1020@gmail.com>
2024-03-17 14:44:00 +0330

    Onion_CQRS - Web API & Infrastructure

---
+++ Architecture/Onion_CQRS/Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Application;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence;
+
+public class AppDbContext : DbContext, IAppDbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Student> Students { get; set; }
+
+    public async Task<int> SaveChangesAsync()
+    {
+        return await base.SaveChangesAsync();
+    }
+}
