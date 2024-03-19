042658c518eb79f2b18a04271b6761422df98daa
Reza <mrdev1020@gmail.com>
2024-03-19 20:02:26 +0330

    Assembly helper

--- Architecture/DDD/Framework/AssemblyHelper/AssemblyHelper.cs
+++ Architecture/DDD/Framework/AssemblyHelper/AssemblyHelper.cs
@@ -1,11 +1,32 @@
 ﻿using Framework.Core.AssemblyHelper;
+using System.Reflection;
 
 namespace Framework.AssemblyHelper;
 
 public class AssemblyHelper : IAssemblyDiscovery
 {
+    private static List<Assembly> loadedAssemblies = null;
+    private readonly string assemblySearchPattern;
+
+    public AssemblyHelper(string assemblySearchPattern)
+    {
+        this.assemblySearchPattern = assemblySearchPattern;
+        LoadAssemblies(assemblySearchPattern);
+    }
+
     public IEnumerable<T> DiscoveryInstances<T>(string searchNamespace)
     {
         throw new NotImplementedException();
+    }
+
+    private void LoadAssemblies(string assemblySearchPattern)
+    {
+        if (loadedAssemblies == null)
+        {
+            var directory = AppDomain.CurrentDomain.BaseDirectory;
+            loadedAssemblies = Directory.GetFiles(directory, assemblySearchPattern)
+                .Select(Assembly.LoadFrom)
+                .ToList();
+        }
     }
 }
