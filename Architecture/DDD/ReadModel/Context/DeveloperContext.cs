2aa12ab898e4b08b307d74e3f7c9d4aae5b0b307
Reza <mrdev1020@gmail.com>
2024-03-19 20:27:24 +0330

    ReadModel Context

---
+++ Architecture/DDD/ReadModel/Context/DeveloperContext.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReadModel.Context.Models;
+
+namespace ReadModel.Context;
+
+public class DeveloperContext : DbContext
+{
+    public DeveloperContext()
+    {
+        
+    }
+
+    public DeveloperContext(DbContextOptions<DeveloperContext> options)
+        : base(options)
+    {
+        
+    }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            //optionsBuilder.UseSqlServer("Server=192.168.2.252;Database=HR_Developer;Trusted_Connection=True;User Id = saleadmin; Password = 123");
+        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.ToTable("Employee", "EmployeeContext");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+
+            entity.Property(e => e.FirstName)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            entity.Property(e => e.LastName)
+                .IsRequired()
+                .HasMaxLength(100);
+        });
+
+    }
+
+    //partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Architecture/DDD/ReadModel/Context/Models/Employee.cs
@@ -0,0 +1,8 @@
+﻿namespace ReadModel.Context.Models;
+
+public class Employee
+{
+    public long Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
